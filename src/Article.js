614657b5b5d255bbf47fef129bdb500c32e61163
Ivan Rastvorov <rastvorov_is@soshace.com>
2016-07-05 12:59:02 -0700

    add stateful Article component

--- src/Article.js
+++ src/Article.js
@@ -1,5 +1,44 @@
-import React from 'react'
+import React, { Component } from 'react'
 
+class Article extends Component {
+    state = {
+        isOpen: false
+    }
+
+/*
+    constructor(props) {
+        super(props)
+        this.state = {
+            isOpen: false
+        }
+    }
+*/
+
+    render() {
+        const article = this.props.article
+//        const { article } = this.props
+//    const { article: { title, text } } = props
+        const { isOpen } = this.state
+        const body = isOpen ? <section>{ article.text }</section> : null
+
+        return (
+            <div>
+                <h1 onClick = {this.toggleOpen}>{ article.title }</h1>
+                {body}
+            </div>
+        )
+    }
+
+    toggleOpen = (ev) => {
+        this.setState({
+            isOpen: !this.state.isOpen
+        })
+    }
+}
+
+
+
+/*
 function Article(props) {
     const article = props.article
 //    const { article: { title, text } } = props
@@ -11,5 +50,6 @@ function Article(props) {
         </div>
     )
 }
+*/
 
 export default Article
--- src/ArticleList.js
+++ src/ArticleList.js
@@ -5,7 +5,6 @@ function ArticleList(props) {
     const { articles } = props
 
     const listItems = articles.map((article) => <li key = {article.id}><Article article = {article}/></li>)
-    console.log('---', listItems)
     return (
         <div>
             <h1>Article list</h1>
