a2c11104a5d341e41a8430802755ee70a3e1c746
Ivan Rastvorov <rastvorov_is@soshace.com>
2016-07-05 11:12:48 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path')
+var webpack = require('webpack')
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './src/app.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?/,
+                loaders: ['babel'],
+                include: path.join(__dirname, 'src')
+            }
+        ]
+    }
+}
