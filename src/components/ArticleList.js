701824a0733c9a14be046fcfb4ca634ac4053237
Ivan Rastvorov <rastvorov_is@soshace.com>
2016-07-15 21:11:45 +0300

    add daypicker

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -3,11 +3,15 @@ import Article from './Article/index'
 import oneOpen from '../decorators/oneOpen'
 import Select from 'react-select'
 import 'react-select/dist/react-select.css'
+import DayPicker, { DateUtils } from "react-day-picker"
+import 'react-day-picker/lib/style.css'
 
 class ArticleList extends Component {
 
     state = {
-        selectedArticles: null
+        selectedArticles: null,
+        from: null,
+        to: null
     }
 
     render() {
@@ -27,17 +31,37 @@ class ArticleList extends Component {
         return (
             <div>
                 <h1>Article list</h1>
+                {this.getRangeTitle()}
                 <Select
                     options = {options}
                     multi = {true}
                     value = {this.state.selectedArticles}
                     onChange = {this.handleSelectChange}
                 />
+                <DayPicker
+                    ref="daypicker"
+                    selectedDays={day => DateUtils.isDayInRange(day, this.state)}
+                    onDayClick={this.handleDayClick}
+                />
                 <ul>
                     {listItems}
                 </ul>
             </div>
         )
+    }
+
+    getRangeTitle() {
+        const { from, to } = this.state
+        const fromText = from && `Start date: ${from.toDateString()}`
+        const toText = to && `Finish date: ${to.toDateString()}`
+
+        return <p>{fromText} {toText}</p>
+    }
+
+    handleDayClick = (e, day) => {
+        console.log('---', day)
+        const range = DateUtils.addDayToRange(day, this.state);
+        this.setState(range)
     }
 
     handleSelectChange = (selectedArticles) => {
