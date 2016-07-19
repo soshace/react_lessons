43437ffa15ca243757bc6d74be5ac504702d1722
Ivan Rastvorov <rastvorov_is@soshace.com>
2016-07-19 23:16:28 +0300

    add dumbMiddleware

--- src/store/index.js
+++ src/store/index.js
@@ -2,8 +2,10 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import reducer from '../reducer'
 import logger from '../middlewares/logger'
 
+const dumbMiddleware = store => next => action => next({...action, addition: 'hello world'})
+
 const enhancer = compose(
-    applyMiddleware(logger),
+    applyMiddleware(dumbMiddleware, logger),
     window.devToolsExtension ? window.devToolsExtension() : f => f
 )
 
