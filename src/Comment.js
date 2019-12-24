3cbaba5be2b4bdfa9598c0f66e4179f5a1a9bb30
Shad Mirza <iamshadmirza@gmail.com>
2019-12-24 06:42:47 +0530

    PropTypes added to Comment

--- src/Comment.js
+++ src/Comment.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import PropTypes from 'prop-types';
 
 function Comment(props) {
     const { comment: { text, user } } = props
@@ -8,6 +9,13 @@ function Comment(props) {
             <b>by {user}</b>
         </div>
     )
+}
+
+Comment.propTypes = {
+    comment: PropTypes.shape({
+        text: PropTypes.string.isRequired,
+        user: PropTypes.string
+    })
 }
 
 export default Comment
