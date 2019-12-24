4c6f01e26cec1ad8bf0c7307a32cb0309a303863
Shad Mirza <iamshadmirza@gmail.com>
2019-12-24 07:22:25 +0530

    react-select added

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import Article from './Article';
 import oneOpen from '../decorators/oneOpen';
+import Select from "react-select";
 
 class ArticleList extends Component {
+    state = {
+        selectedArticles: null
+    };
+    handleSelectChange = selectedArticles => {
+        console.log(selectedArticles);
+        this.setState({ selectedArticles });
+    };
     renderListItem = () => {
         const { articles, isItemOpen, toggleOpenItem } = this.props;
         return articles.map((article) => (
@@ -17,9 +25,20 @@ class ArticleList extends Component {
     };
 
     render() {
+        const { articles } = this.props;
+        const options = articles.map(article => ({
+            label: article.title,
+            value: article.id
+        }));
         return (
             <div>
                 <h1>Article list</h1>
+                <Select
+                    options={options}
+                    isMulti={true}
+                    value={this.state.selectedArticles}
+                    onChange={this.handleSelectChange}
+                />
                 <ul>
                     {this.renderListItem()}
                 </ul>
