debc1147adbb2ab4237fee7503faecee19e2e4eb
Shad Mirza <iamshadmirza@gmail.com>
2019-12-24 07:13:55 +0530

    mixins added

---
+++ src/mixins/oneOpen.js
@@ -0,0 +1,26 @@
+export default {
+  getInitialState() {
+    return {
+      openItemId: false
+    }
+  },
+  openItem(openItemId) {
+    return ev => {
+      if (ev) ev.preventDefault()
+      this.setState({ openItemId });
+    }
+  },
+
+  toggleOpenItem(id) {
+    return ev => {
+      if (ev) ev.preventDefault();
+      this.setState({
+        openItemId: id === this.state.openItemId ? null : id
+      });
+    }
+  },
+
+  isItemOpen(id) {
+    return this.state.openItemId === id;
+  }
+}
