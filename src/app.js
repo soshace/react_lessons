a3b561ec56db54641f2bf73d76d8531ca90b19c3
Shad <iamshadmirza@gmail.com>
2019-12-29 15:29:22 +0530

    package dependency fix

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-import { articles } from './fixtures';
-import ArticleList from './components/ArticleList';
 import React from 'react';
-import { render } from 'react-dom';
+import ArticleList from './components/ArticleList';
+import { articles } from './fixtures';
 
-render(
-  <ArticleList articles={articles} />,
-  document.getElementById('container')
-);
+function App() {
+  return (
+    <ArticleList articles={articles} />
+  );
+}
+
+export default App;
