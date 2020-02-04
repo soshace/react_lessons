5d9945bc3ce21d048f171c094b13653d6f6d6394
Shad <iamshadmirza@gmail.com>
2020-02-04 12:31:11 +0530

    repo updated for lesson 11.2

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import reducer from "../reducers";
+import logger from "../middlewares/logger";
 
-const store = createStore(reducer);
+const dumbMiddleware = store => next => action =>
+  next({ ...action, addition: "hello world" });
+
+const enhancer = compose(
+  applyMiddleware(dumbMiddleware, logger),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+);
+
+const store = createStore(reducer, {}, enhancer);
 
 export default store;
