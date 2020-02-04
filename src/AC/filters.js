b19a823376940914ed07493f603fd4bec52dce11
Shad <iamshadmirza@gmail.com>
2020-02-04 16:05:37 +0530

    removed old unused folders

--- src/AC/filters.js
+++
@@ -1,9 +0,0 @@
-import { CHANGE_FILTERS } from '../constants'
-
-
-export function changeFilters(change) {
-    return {
-        type: CHANGE_FILTERS,
-        payload: { change }
-    }
-}
--- src/reducer/counter.js
+++
@@ -1,5 +0,0 @@
-import { INCREMENT } from '../constants'
-
-export default (count = 0, action) => {
-    return action.type == INCREMENT ? count + 1 : count
-}
