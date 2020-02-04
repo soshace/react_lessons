6148cf911c987d97d82ae7ea67f3e335be4140e9
Shad <iamshadmirza@gmail.com>
2020-02-04 12:21:28 +0530

    removed redundant folder

--- src/reducer/index.js
+++
@@ -1,8 +0,0 @@
-import articles from './articles'
-import counter from './counter'
-import { combineReducers } from 'redux'
-
-export default combineReducers({
-    count: counter,
-    articles
-})
